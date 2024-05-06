c3a12718a3f4a61c18d22bdc67dbc2b2b0e46e5f
pedrohenrique57 <pedro.henrique.contato369@gmail.com>
2024-05-06 13:09:12 -0300

    Conexão com banco para teste
    Signed-off-by: pedrohenrique57 <pedro.henrique.contato369@gmail.com>

--- banco__clean/src/main/java/Edu/Syne/Main.java
+++ banco__clean/src/main/java/Edu/Syne/Main.java
@@ -1,7 +1,16 @@
 package Edu.Syne;
 
+import Edu.Syne.external.DB.MySQLConnector;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws SQLException, IOException {
+
+    Connection db = MySQLConnector.openConnection();
+
+    System.out.println(db.createStatement().execute("select * from banco1"));
   }
 }
